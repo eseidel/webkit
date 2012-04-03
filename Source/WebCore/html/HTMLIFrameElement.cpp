6e3b6855f415f5207e34e5e46bd2a5931fef02f8
Eric Seidel <github@eseidel.com>
2012-04-03 01:53:39 -0700

    Check mayDisplaySeamless on the correct document

--- Source/WebCore/html/HTMLIFrameElement.cpp
+++ Source/WebCore/html/HTMLIFrameElement.cpp
@@ -118,7 +118,7 @@ void HTMLIFrameElement::removedFromDocument()
 
 bool HTMLIFrameElement::shouldDisplaySeamlessly() const
 {
-    return hasAttribute(seamlessAttr) && document()->mayDisplaySeamlessWithParent();
+    return hasAttribute(seamlessAttr) && contentDocument() && contentDocument()->mayDisplaySeamlessWithParent();
 }
 
 #if ENABLE(MICRODATA)
